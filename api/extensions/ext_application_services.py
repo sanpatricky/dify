1e486292ee8d018d14eb819a5379d65f2d6667f6
hjlarry <hjlarry@163.com>
2026-08-09 14:40:54 +0800

    refactor(api): decouple recommended app runtime admission

--- api/extensions/ext_application_services.py
+++ api/extensions/ext_application_services.py
@@ -24,7 +24,6 @@ from services.feature_service import FeatureService
 from services.feature_service_gateway import FeatureServiceGateway
 from services.recommended_app_query_compat import LegacyRecommendedAppCatalogGateway
 from services.recommended_app_query_service import RecommendedAppQueryService
-from services.recommended_app_service import RecommendedAppService
 from services.schema_definition_service import SchemaDefinitionService
 from services.setup_adapters import RedisSetupLock, RegisterServiceAccountProvisioner
 from services.setup_service import SetupService
@@ -76,7 +75,7 @@ def build_application_services(
         recommended_app_queries=RecommendedAppQueryService(
             catalog=LegacyRecommendedAppCatalogGateway(session_factory=database_client),
             trial_apps=TrialAppQueryRepository(session_factory=database_client),
-            is_trial_enabled=RecommendedAppService.is_trial_app_enabled,
+            is_trial_enabled=FeatureService.is_trial_app_enabled,
         ),
         trial_app_usage=TrialAppUsageService(
             usage=TrialAppUsageRepository(session_factory=database_client),
